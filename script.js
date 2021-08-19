23c28de234646cf250448076ddd36cb1628764a5
Adam <adam.haldas@gmail.com>
2021-08-19 20:38:57 +0200

    add glass nav effect

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation__list');
 let listItems = document.querySelectorAll('a.navigation__item');
+const nav = document.querySelector('nav');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('hamburger--active');
